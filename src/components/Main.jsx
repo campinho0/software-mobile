0e4005c7bd2242838dc8fa35e1453945cf82a7b9
vacabe23 <valenbe2216@gmail.com>
2024-05-14 20:03:42 -0500

    change on logo app

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,30 +1,74 @@
 import React from 'react';
-import { Button, View, Image, Text, StyleSheet } from 'react-native';
+import { View, Image, Text, StyleSheet } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Button } from 'react-native-paper';
 
 const Main = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Image source={require('../resources/logo.jpg')} style={styles.image} />
-      <Button
-            title="See vehicles"
-            onPress={() => navigation.navigate('Catalog')}
-            />
+      <ScrollView>
+        <View style={styles.container}>
+          <Image source={require('../resources/logo.png')} style={styles.image} />
+        </View>
+        <View style={styles.containerSlogan}>
+          <Text style={styles.tittle}>Eslogan</Text>
+        </View>
+        <View>
+          
+            <Button
+              style={styles.button}
+              labelStyle= {styles.buttonLabel}
+              mode='contained'
+              onPress={() => navigation.navigate('Catalog')}>
+              Ver vehiculos
+            </Button>
+            <Button
+              style={styles.button}
+              mode='contained'
+              onPress={() => navigation.navigate('Catalog')}>
+              Test drive
+            </Button>
+            <Button
+              style={styles.button}
+              mode='contained'
+              onPress={() => navigation.navigate('Catalog')}>
+              Servicio mecanico
+            </Button>
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#2F3135',
-        justifyContent: 'center',
-        alignItems: 'center'
-      },
-    image: {
-        width: 500,
-        height: 300,
-        backgroundColor: 'gray'
-      },
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width:500
+  },
+  containerSlogan: {
+    height:500
+  },
+  image: {
+    width: 300,
+    height: 300,
+    backgroundColor: 'white'
+  },
+  button: {
+    borderRadius: 50,
+    backgroundColor: 'black',
+    margin: 10,
+    padding: 10
+  },
+  buttonLabel: {
+    fontSize: 20
+  },
+  tittle: {
+    fontSize: 50,
+    color: 'black'
+  },
 
 });
 
